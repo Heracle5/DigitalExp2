3c1e573b7df081a6cb2ba9b2a60dc8bcd248446d
Heracle5 <marxlenin2001@gmail.com>
2021-12-05 22:36:11 +0800

    commit

--- exp_elevator/key_scanning.v
+++ exp_elevator/key_scanning.v
@@ -1,8 +1,16 @@
 module key_scanning(input[3:0] col,
-input clk,
-output reg[3:0] row,
+input clk_scan,
+input clk_50,
+output reg[3:0] row=4'b0001,
 output up_outside,
 output down_outside,
 output up_inside,
 output down_inside
 );
+reg[15:0] btn=0;
+always@(posedge clk_scan) 
+begin
+    if (col == 0) begin
+        up_outside <= 1;
+        down_outside <= 1;
+        up_inside <= 0;
