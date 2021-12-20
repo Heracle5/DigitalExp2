91aaee654c11fcc129c08dffabc338f62241565e
Heracle5 <marxlenin2001@gmail.com>
2021-12-20 20:59:01 +0800

    initial finish

--- exp_elevator/final_project.srcs/sources_1/new/LogicProcessingUnited.v
+++ exp_elevator/final_project.srcs/sources_1/new/LogicProcessingUnited.v
@@ -1,24 +1,23 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 2021/12/19 23:32:27
-// Design Name: 
+// Design Name:
 // Module Name: LogicProcessingUnited
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
-
 
 module LogicProcessingUnited(
 input up,
@@ -33,98 +32,111 @@ output reg [1:0] state,
 output reg [1:0] floor,
 output reg buzzer
 );
+reg [3:0] flag=0;
 reg [31:0] cnt=0;
 always@(posedge clk_50mhz)
 begin
 if(!rst)
 begin
-    led_drive<=0;
-    state<=0;
-    floor<=1;
+led_drive<=0;
+state<=0;
+floor<=1;
 end
 else if(start_stop)
 begin
-    if(led_drive)
-    begin
-        if(cnt==199999999)
-        begin
-            cnt<=0;
-            if(floor==1)
-            begin
-              floor=2;
-            end
-            else
-            begin
-              floor=1;
-            end
-            state=0;//00000010
-            led_drive=0;
-            buzzer=1;
-        end
-        else
-        begin
-            cnt=cnt+1;
-            buzzer=0;
-            if((state==1)&&(toOne))
-            begin
-            led_drive=led_drive+4'b0100;
-            flag=1;
-            end
-            else if((state==1)&&(up))
-            begin
-            led_drive=led_drive+4'b0010;
-            flag=1//ä¸‹è¡Œ
-            end
-            else if((state==2)&&(toTwo))
-            begin
-            led_drive=led_drive+4'b1000;
-            flag=2
-            end
-            else if((state==2)&&(down))
-            begin
-            led_drive=led_drive+4'b0001;
-            flag=2;
-            end
-        end
-    end
-    else if((down)&&(state==0))//floor==1
-    begin
-        if(floor!=1)//==2
-        begin
-            state=1;//00010000
-            led_drive=4'b0001;
-        end
-    end
-    else if((up)&&(state==0))
-    begin
-        if(floor!=2)
-        begin
-            state=2;//10000000
-            led_drive=4'b0010;
-        end
-    end
-    else if((toOne)&&floor==2)
-    begin
-        state=1;//00010000
-        led_drive=4'b0100;
-    end
-    else if((toTwo)&&floor==1)
-    begin
-        state=2;//10000000
-        led_drive=4'b1000;
-    end
-    else if(flag==1)
-    begin
-      flag=0;
-      state=2;
-      led_drive=led_drive-4'b1000;
-    end
-    else if(flag==2)
-    begin
-      flag=0;
-      state=1;
-      led_drive=led_drive-4'b0100;
-    end
+if(led_drive)
+begin
+if(cnt==199999999)
+begin
+cnt<=0;
+if(floor==1)
+begin
+floor=2;
+end
+else
+begin
+floor=1;
+end
+state=0;//00000010
+led_drive=0;
+buzzer=1;
+end
+else
+begin
+cnt=cnt+1;
+buzzer=0;
+if((led_drive==4'b1000)&&(toOne))
+begin
+led_drive=4'b1100;
+flag=1;
+end
+else if((led_drive==4'b1000)&&(down))
+begin
+led_drive=4'b1001;
+flag=2;//ÏÂÐÐ
+end
+else if((led_drive==4'b0100)&&(toTwo))
+begin
+led_drive=4'b1100;
+flag=3;
+end
+else if((led_drive==4'b0100)&&(up))
+begin
+led_drive=4'b0110;
+flag=4;
+end
+end
+end
+else if((down)&&(state==0))//floor==1
+begin
+if(floor!=1)//==2
+begin
+state=1;//00010000
+led_drive=4'b0001;
+end
+end
+else if((up)&&(state==0))
+begin
+if(floor!=2)
+begin
+state=2;//10000000
+led_drive=4'b0010;
+end
+end
+else if((toOne)&&floor==2)
+begin
+state=1;//00010000
+led_drive=4'b0100;
+end
+else if((toTwo)&&floor==1)
+begin
+state=2;//10000000
+led_drive=4'b1000;
+end
+else if(flag==1)
+begin
+flag=0;
+state=1;
+led_drive=4'b0100;
+end
+else if(flag==2)
+begin
+flag=0;
+state=1;
+led_drive=4'b0001;
+end
+else if(flag==3)
+begin
+flag=0;
+state=2;
+led_drive=4'b1000;
+end
+else if(flag==4)
+begin
+flag=0;
+state=2;
+led_drive=4'b0010;
+end
 end
 end
 endmodule
